196fe07f91b8de9b7d5b3e5de9f43a4167e1ae0f
Nick Bever <nicholasbever@Nicholass-MacBook-Air-2.local>
2021-04-23 17:28:54 -0700

    sign in and sign up method complete

--- src/components/sign-in/sign-in.component.tsx
+++ src/components/sign-in/sign-in.component.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -22,10 +22,16 @@ class SignIn extends React.Component<any,SignInState | any> {
     }
   }
 
-  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    const { email, password } = this.state;
 
-    this.setState({ email: '', password: ''})
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: ''});
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   handleChange =(event: React.FormEvent<HTMLInputElement> | any) => {
